dd312664b1a8f3e442644e21cbcb1edcea06a102
ch04937 <97hernandez.c@gmail.com>
2021-07-24 16:44:27 -0700

    connect mongo server

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-
-const auth = require("./src/routes/router/auth");
-const profile = require("./src/routes/router/profile");
-const alliance = require("./src/routes/router/alliance");
-const event = require("./src/routes/router/event");
-
-const port = process.env.PORT || 4000;
-
+const mongoose = require("mongoose");
 const server = express();
 
-server.use(express.static("./public"));
+// connect to mongodb
+const dbURI = `mongodb+srv://${process.env.MG_USER}:${process.env.MG_PASSWORD}@cluster0.08fu7.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+const port = process.env.PORT || 04937;
 
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => server.listen())
+  .catch((err) => console.log("err", err));
+
+server.use(express.static("./public"));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
-server.use("/user", auth);
-server.use("/profile", profile);
-server.use("/alliance", alliance);
-server.use("/event", event);
-server.use("/public", express.static("./public"));
 
 server.get("/", (req, res) => res.send("index"));
 
--- src/utils/secrets.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	jwtSecrets: process.env.JTW_SECRET || "keep it secret, keep it safe",
-	jwtRefreshSecrets: process.env.JTW_SECRET2 || "SO_SECRET",
-};
