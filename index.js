3d07eeee817f4652ef26f561f2f65ce91f9821e7
ch04937 <97hernandez.c@gmail.com>
2021-08-02 09:39:59 -0700

    connection 200

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const helmet = require("helmet");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const server = express();
+const usersRouter = require("./router/users");
 
-// connect to mongodb
-const dbURI = `mongodb+srv://${process.env.MG_USER}:${process.env.MG_PASSWORD}@cluster0.08fu7.mongodb.net/Cluster0?retryWrites=true&w=majority`;
-const port = process.env.PORT || 04937;
-
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => server.listen())
-  .catch((err) => console.log("err", err));
-
-server.use(express.static("./public"));
+// express app
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use("/users", usersRouter);
 
-server.get("/", (req, res) => res.send("index"));
+// connect to mongodb
+const dbURI = `mongodb+srv://${process.env.MG_USER}:${process.env.MG_PASSWORD}@cluster0.08fu7.mongodb.net/Cluster0?retryWrites=true&w=majority`;
+const port = process.env.PORT;
 
-server.listen(port, () => console.log(`\n*** Listening on port ${port}***\n`));
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    server.listen(port, () =>
+      console.log(`\n*** Listening on port ${port}***\n`)
+    )
+  )
+  .catch((err) => console.log("err", err));
+
+server.get("/", (req, res) => {
+  res.status(202).send({
+    api: "Rise of Kingdoms api",
+    paths: ["/users/", "/alliance/"],
+  });
+});
---
+++ models/models.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    discordUserId: {
+      type: Number,
+      required: true,
+    },
+    discordServerId: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const eventSchema = new Schema(
+  {
+    eventName: {
+      type: String,
+      required: true,
+    },
+    discordServerId: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+const Events = mongoose.model("Events", eventSchema);
+
+module.exports = { User, Events };
